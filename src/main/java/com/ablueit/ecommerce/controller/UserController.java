7830f3fc5846ddded893df58fc23bbc3bcd48e52
vandunxg <nguyenvandung.official@gmail.com>
2025-06-09 15:30:28 +0700

    fix home

--- src/main/java/com/ablueit/ecommerce/controller/UserController.java
+++ src/main/java/com/ablueit/ecommerce/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.ablueit.ecommerce.controller;
 
+import com.ablueit.ecommerce.enums.ImageType;
 import com.ablueit.ecommerce.model.Categories;
 import com.ablueit.ecommerce.model.Product;
+import com.ablueit.ecommerce.model.ProductImage;
 import com.ablueit.ecommerce.model.Store;
+import com.ablueit.ecommerce.payload.response.ProductCardResponse;
 import com.ablueit.ecommerce.repository.CategoriesRepository;
+import com.ablueit.ecommerce.repository.ProductImageRepository;
 import com.ablueit.ecommerce.repository.ProductRepository;
 import com.ablueit.ecommerce.repository.StoreRepository;
 import org.slf4j.Logger;
@@ -27,17 +31,35 @@ public class UserController {
     @Autowired
     private CategoriesRepository categoriesRepository;
 
+    @Autowired
+    private ProductImageRepository productImageRepository;
+
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/")
     public String home(Model model) {
         List<Product> topProducts = productRepository.findTop8ByOrderBySalesCountDesc();
+
+        List<ProductCardResponse> cards = topProducts.stream().map(x -> {
+            ProductImage primaryImage = productImageRepository
+                    .findByImageTypeAndProduct(ImageType.PRIMARY, x);
+
+            return ProductCardResponse.builder()
+                    .id(x.getId())
+                    .name(x.getName())
+                    .price(x.getPrice())
+                    .rating(4.5)
+                    .primaryImage(primaryImage.getUrl())
+                    .totalSold(100L)
+                    .build();
+        }).toList();
+
         List<Store> stores = storeRepository.findAll();
 
         List<Categories> randomCategories = categoriesRepository.findAll().stream().limit(10).toList();
 
         model.addAttribute("categories", randomCategories);
-        model.addAttribute("products", topProducts);
+        model.addAttribute("products", cards);
         model.addAttribute("stores", stores);
 
         return "index"; // Trả về file index.html trong templates
